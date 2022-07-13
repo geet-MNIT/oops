d08dcacf529d508f22b49655665d54f6390df037
geet-MNIT <107451212+geet-MNIT@users.noreply.github.com>
2022-07-13 17:20:48 +0530

    Create util1.py

---
+++ util1.py
@@ -0,0 +1,6 @@
+class candidate_next:
+    def __init__(self, name, surname, emailid, yob):
+        self._name=name
+        self.__surname= surname
+        self.emailid=emailid
+        self.yob=yob
